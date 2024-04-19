d9c2be6c86c10b948ae011eeac394dfdd40e1e0c
ohara <kzAme79@gmail.com>
2024-04-20 03:59:39 +0900

    complete sidebar

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,6 +1,7 @@
 import { BsBellFill, BsHouseFill } from "react-icons/bs";
 import { FaUser } from "react-icons/fa";
 import SidebarLogo from "./SidebarLogo";
+import SidebarItem from "./SidebarItem";
 
 const Sidebar = () => {
   const items = [
@@ -25,6 +26,14 @@ const Sidebar = () => {
       <div className="flex flex-col items-end">
         <div className="space-y-2 lg:w-[230px]">
           <SidebarLogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+            />
+          ))}
         </div>
       </div>
     </div>
